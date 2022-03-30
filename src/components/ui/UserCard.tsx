237671e9419cbc00ecb5c7f3c8e0e833c236a4dd
yamadahiroki <hirockysan1983@gmail.com>
2022-03-30 15:46:51 +0900

    🎨 SNSのセクション追加

--- src/components/ui/UserCard.tsx
+++ src/components/ui/UserCard.tsx
@@ -1,15 +1,17 @@
 import { Card, Text, useMantineTheme, Avatar } from "@mantine/core";
 import { VFC } from "react";
 import { Profile } from "../../type/type";
+import { BrandTiktok, BrandFacebook } from "tabler-icons-react";
+import Link from "next/link";
 
 export const UserCard: VFC<Profile> = (props) => {
-  const { name, avater, discription } = props;
+  const { name, avater, discription, facebookLink, tiktokLink } = props;
   const theme = useMantineTheme();
 
   const secondaryColor =
     theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[7];
   return (
-    <div className="w-60 p-auto fixed top-24 right-6">
+    <div className="w-60 p-auto fixed top-44 right-12">
       <Card shadow="sm" p="lg">
         <Card.Section className="flex items-center p-4">
           <Avatar radius="xl" size={80} src={avater.url} />
@@ -17,10 +19,25 @@ export const UserCard: VFC<Profile> = (props) => {
             {name}
           </Text>
         </Card.Section>
-
         <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
           {discription}
         </Text>
+        <Card.Section className="flex items-center p-4 gap-2">
+          {tiktokLink && (
+            <Link href={tiktokLink}>
+              <a target="_blank">
+                <BrandTiktok size={30} strokeWidth={2} />
+              </a>
+            </Link>
+          )}
+          {facebookLink && (
+            <Link href={facebookLink}>
+              <a target="_blank">
+                <BrandFacebook size={30} strokeWidth={2} />
+              </a>
+            </Link>
+          )}
+        </Card.Section>
       </Card>
     </div>
   );
