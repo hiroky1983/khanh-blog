2b13be1df1a9d29734a8e7adb0d9e22283acfb68
yamadahiroki <hirockysan1983@gmail.com>
2022-03-30 10:24:15 +0900

    🎨 画面右側のスタイリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { ListCard } from "../components/ui/ListCard";
 import { UserCard } from "../components/ui/UserCard";
 import { Blog, Profile } from "../type/type";
 import { Month } from "@mantine/dates";
-import { AdjustmentsHorizontal } from "tabler-icons-react";
+import { SearchSecction } from "../components/ui/SearchSecction";
 
 type Props = {
   blog: Blog[];
@@ -17,13 +17,8 @@ const Home: NextPage<Props> = ({ blog, profile }) => {
     <>
       <div className="flex py-6">
         <ul className="grid flex-grow justify-center gap-2 pt-16">
-          <AdjustmentsHorizontal
-            size={34}
-            strokeWidth={1}
-            color={"#12B981"}
-            className="hover:bg-opacity-70 hover:bg-gray-200 rounded-lg hover:cursor-pointer"
-          />
-          {blog.map(({ id, title, eyeCatchImage, createdAt, tag }, i) => (
+          <SearchSecction />
+          {blog.map(({ id, title, eyeCatchImage, createdAt, tag }) => (
             <>
               <li key={id}>
                 <Link href={`/blog/${id}`}>
@@ -46,7 +41,7 @@ const Home: NextPage<Props> = ({ blog, profile }) => {
             avater={profile.avater}
             discription={profile.discription}
           />
-          <Month month={new Date()} className="fixed top-72 right-4" />
+          <Month month={new Date()} className="fixed top-72 right-6" />
         </div>
       </div>
     </>
