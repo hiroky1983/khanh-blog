f972864973a2a5b79bd1df35fa1e92e5d55c224a
yamadahiroki <hirockysan1983@gmail.com>
2022-04-29 16:09:12 +0900

    お問い合わせのリンク作成

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,22 +1,19 @@
 import { Select } from "@mantine/core";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="flex bg-green-500 text-white w-screen py-4 px-8">
       <h1 className="flex flex-grow font-bold text-xl">Blog</h1>
-      {/* <nav>
+      <nav>
         <ul className="flex gap-2">
           <li>
-            <Select
-              placeholder="言語設定"
-              data={[
-                { value: "日本語", label: "日本語", lang: "ja" },
-                { value: "ベトナム語", label: "viet-nam", lang: "vi" },
-              ]}
-            />
+            <Link href={"/contact"}>
+              <a>お問い合わせ</a>
+            </Link>
           </li>
         </ul>
-      </nav> */}
+      </nav>
     </header>
   );
 };
