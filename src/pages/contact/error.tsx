83ac63c608564a1e59afedb525a129e8a672101d
yamadahiroki <hirockysan1983@gmail.com>
2022-05-04 23:58:48 +0900

    お問い合わせ完了時の設定

--- src/pages/contact/error.tsx
+++ src/pages/contact/error.tsx
@@ -1,9 +1,18 @@
+import { Image } from "@mantine/core";
 import { NextPage } from "next";
+
+import Link from "next/link";
 
 export const error: NextPage = () => {
   return (
     <div>
       <h1>error</h1>
+
+      <Image src="/error.svg" alt="error" width={300} height={300} />
+
+      <Link href="/">
+        <a>Home</a>
+      </Link>
     </div>
   );
 };
--- src/pages/contact/success.tsx
+++ src/pages/contact/success.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from "next";
+import Link from "next/link";
 
 export const success: NextPage = () => {
   return (
     <div>
       <h1>Success</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
     </div>
   );
 };
