0e4999a70360b44c8e7bfd60ec83031de3f1ae33
yamadahiroki <hirockysan1983@gmail.com>
2022-05-22 16:33:54 +0900

    svgの表示設定

--- src/pages/contact/success.tsx
+++ src/pages/contact/success.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 
 export const success: NextPage = () => {
   return (
-    <div>
-      <h1>Success</h1>
-      <Image src="/success.svg" alt="success" width={300} height={300} />
+    <div className="flex flex-col items-center gap-4">
+      <h2 className="text-3xl font-bold text-green-700">Success!</h2>
+      <Image src="/success.svg" alt="success" />
+      <p>送信に成功しました。</p>
       <Link href="/">
         <a className="text-blue-500 underline">Homeにもどる</a>
       </Link>
